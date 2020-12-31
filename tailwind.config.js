c3b51d5b4de9f7895320d6cf3550d6921a0d8d73
Lucas Encarnacao <lucas.encarnacao5@gmail.com>
2020-12-31 17:39:02 -0500

    Added modal skeleton

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,30 @@
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        popIn: {
+          "0%": {
+            transform: "translateY(1em)",
+            opacity: "10%",
+          },
+          "60%": {
+            transform: "translateY(-0.5em)",
+          },
+          "100%": {
+            transform: "translateY(0)",
+            opacity: "100%",
+          },
+        },
+      },
+      animation: {
+        popIn: "popIn 0.5s ease-in-out",
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
